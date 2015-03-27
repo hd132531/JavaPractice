09404ae8671ba3cafda3963c9198a69c804f4f53
정희조 <@user>
2015-03-27 14:09:43 +0900

    다섯번째 과재제출

---
+++ javapractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,19 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		for(int k=0;k<10;k++){
+			for(int i=0;i<10;i++){
+				if(k!=i){
+					System.out.print(i+1);
+				}
+				else System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
