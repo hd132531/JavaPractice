c4204873a6c570464f4eb308823a787e9b53c95c
정희조 <@user>
2015-04-13 23:33:49 +0900

    일곱번째 과제제출

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ *org.dimigo.oop
+ *		|_ Car
+ *
+ *
+ *1. 개요 :
+ *2. 작성일 : 2015. 4. 13.
+ *</pre>
+ * @author :정희조
+ *
+ * @version :1.0
+ */
+public class Car {
+	private	String company;
+	private	String model;
+	private	String color;
+	private	int maxSpeed;
+	private	int price;
+	public 	String getCompany(){
+	return company;
+	};
+	public		String getModel(){
+		return model;
+	};
+	public		String getColor(){
+		return color;
+	};
+	public		int getMaxSpeed(){
+		return maxSpeed;
+	};
+	public		int getPrice(){
+		return price;
+	}
+	public 	void setCompany(String s){
+		company=s;
+	};
+	public		void setModel(String s){
+		model=s;
+	};
+	public		void setColor(String s){
+		color=s;
+	};
+	public		void setMaxSpeed(int i){
+		maxSpeed=i;
+	};
+	public		void setPrice(int i){
+		price=i;
+	}
+	
+}
