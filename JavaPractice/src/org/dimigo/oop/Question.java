2f952efa591c287d27dcb5ff8dd06c50a68a9aa0
정희조 <@user>
2015-05-11 11:17:35 +0900

    8번째 제출

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *org.dimigo.oop
+ *		|_ Question
+ *
+ *
+ *1. 개요 :
+ *2. 작성일 : 2015. 5. 11.
+ *</pre>
+ * @author :정희조
+ *
+ * @version :1.0
+ */
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String answer;
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("가장 싫어하는 회사는?");
+			answer=scanner.nextLine();
+			if(answer.equals("아라코")){
+				System.out.println("정답입니다!! ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+			}
+			else System.out.println("틀렸습니다!! ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ");
+			System.out.println("가장 재밌는 게임은?");
+			answer=scanner.nextLine();
+			if(answer.equals("라스트 오브 어스")){
+				System.out.println("정답입니다!! ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+			}
+			else System.out.println("틀렸습니다!! ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ");
+			System.out.println("지금 가장 필요한것은?");
+			answer=scanner.nextLine();
+			if(answer.equals("귀가")){
+				System.out.println("정답입니다!! ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+			}
+			else System.out.println("틀렸습니다!! ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ");
+	}
+
+}
