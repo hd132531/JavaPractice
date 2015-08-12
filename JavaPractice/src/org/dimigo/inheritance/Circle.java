73c6f972272dc77db813a9d9a9d2204dc46ac39c
정희조 <@user>
2015-08-12 15:43:22 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/inheritance/Circle.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *		|_ Circle
+ *
+ *
+ *1. 개요 :
+ *2. 작성일 : 2015. 8. 12.
+ *</pre>
+ * @author :정희조
+ *
+ * @version :1.0
+ */
+public class Circle extends Figure{
+	private int radius;
+	public Circle(int radius){
+		this.radius=radius;
+	}
+	public Circle(int centerX,int centerY,int radius){
+		this.radius=radius;
+	}
+	protected double calcArea(){
+		return Math.PI*this.radius*this.radius;
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/Rectangle.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *		|_ Lectangle
+ *
+ *
+ *1. 개요 :
+ *2. 작성일 : 2015. 8. 12.
+ *</pre>
+ * @author :정희조
+ *
+ * @version :1.0
+ */
+public class Rectangle extends Figure{
+	private int width;
+	private int height;
+	public Rectangle(int width,int height){
+		this.width=width;
+		this.height=height;
+	}
+	public Rectangle(int centerX,int centerY,int width,int height){
+		this.width=width;
+		this.height=height;
+	}
+	protected double calcArea(){
+		return width*height;
+	}
+}
