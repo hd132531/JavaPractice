dceb4539b9fcc753d4a3a86a9b0239533fe9f291
정희조 <@user>
2015-09-01 10:16:17 +0900

    ^^

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *		|_ SmartPhoneTest
+ *
+ *
+ *1. 개요 :
+ *2. 작성일 : 2015. 8. 25.
+ *</pre>
+ * @author :정희조
+ *
+ * @version :1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone a=new IPhone("iphone 6","애플",700000);
+		SmartPhone s=new Galaxy("갤럭시 S6","삼성",650000);
+		System.out.println(a);
+		a.turnOn();
+		a.pay();
+		a.useSpecialFuction();
+		a.turnOff();
+		System.out.println();
+		System.out.println(s);
+		s.turnOn();
+		s.pay();
+		s.useSpecialFuction();
+		s.turnOff();
+	}
+
+}
